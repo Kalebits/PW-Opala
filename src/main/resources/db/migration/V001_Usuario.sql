d0530ed10180161892fa4f76b7163e9e4a599f26
Kaleb <Kaleb@example.com>
2023-08-28 11:24:38 -0300

    Finalizacao Migrations

--- src/main/resources/db/migration/V001_Usuario.sql
+++ src/main/resources/db/migration/V001_Usuario.sql
@@ -1,4 +1,3 @@
-use opala
 create table Usuario(
 Id_Usuario numeric(10) constraint Id_Usuario_Pk primary key,
 Nome char (38) not null,
@@ -7,8 +6,8 @@ Senha varchar(38) not null,
 Telefone numeric (11),
 endereco char(30),
 tipoUsuario char(13),
-genero char(10)
+genero char(10) not null
 )
 
-INSERT INTO Usuario (Nome, Email, Senha, Telefone, endereco, tipoUsuario, genero) VALUES ('Pedro Silva', 'PedroS@gmail.com', 'pedrinho123', null, null, null, null);
-INSERT INTO Usuario (Nome, Email, Senha, Telefone, endereco, tipoUsuario, genero) VALUES ('Gustavo Henrique', 'gHenrique@gmail.com', 'henrique123', null, null, null, null);
+INSERT INTO Usuario (Nome, Email, Senha, Telefone, endereco, tipoUsuario, genero) VALUES ('Pedro Silva', 'PedroS@gmail.com', 'pedrinho123', null, null, 'Contratante', 'Masculino');
+INSERT INTO Usuario (Nome, Email, Senha, Telefone, endereco, tipoUsuario, genero) VALUES ('Gustavo Henrique', 'gHenrique@gmail.com', 'henrique123', null, null, 'Artista', 'Masculino');
