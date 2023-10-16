bbb7b6e50b745eecd96e6906e7eef96c14d5fef2
alvessz <fis>
2023-10-16 12:00:15 -0300

    query banda

--- src/main/java/com/etechoracio/opala/repositories/BandaRepository.java
+++ src/main/java/com/etechoracio/opala/repositories/BandaRepository.java
@@ -4,14 +4,17 @@ import com.etechoracio.opala.entity.Banda;
 import com.etechoracio.opala.entity.Midia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
 
+
 public interface BandaRepository extends JpaRepository<Banda, Long> {
 
-    @Query("select b from Banda b INNER JOIN UsuBanda as ub ON " +
-            "b.ID_BANDA = ub.ID_BANDA LEFT JOIN Usuario as u ON ub.ID_USUARIO = u.ID_USUARIO " +
-            "where u.ID_USUARIO = :idUsuario")
-
-    List<Banda> findAllBandas(long idUsuario);
+    @Query("select b from Banda b inner join Usuario ub on b.id = ub.bandas.idBanda " +
+            "where ub.idUsuario = :idUsuario")
+            /*"select b from Banda b INNER JOIN Usuario as u ON " +
+            "b.ID_BANDA = u.bandas.idBanda" +
+            "where u.ID_USUARIO = :idUsuario")*/
+    List<Banda> findAllBandasByUser(Long idUsuario);
 }
